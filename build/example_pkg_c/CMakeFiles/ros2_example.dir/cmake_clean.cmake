7374da1cbd797964f4b79e89eeeacc2f36f5e74c
wang <renjiewang31@gmail.com>
2023-01-17 16:26:59 +0800

    first commit

---
+++ build/example_pkg_c/CMakeFiles/ros2_example.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros2_example.dir/src/marker.cpp.o"
+  "CMakeFiles/ros2_example.dir/src/marker.cpp.o.d"
+  "ros2_example"
+  "ros2_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros2_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/example_pkg_c/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wrdev/workspace/ros2_ws/src/example_pkg_c
+# Build directory: /home/wrdev/workspace/ros2_ws/build/example_pkg_c
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
